442d6649792e97952d60186251f1f74b6b06e023
athens1996 <atharva80000@gmail.com>
2021-09-17 22:16:25 +0530

    Update BookServiceimpl.java

--- src/main/java/com/Third/Final/controller/service/BookServiceimpl.java
+++ src/main/java/com/Third/Final/controller/service/BookServiceimpl.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.Third.Final.controller.dao.BookDao;
 import com.Third.Final.controller.model.Book;
@@ -16,7 +16,7 @@ public class BookServiceimpl implements BookService {
 	
 	@Autowired
 	private BookDao bookDao;
-	//@Transactional
+	@Transactional
 	@Override
 	public List<Book> get() {
 return 	bookDao.get();
